fd07f40666758095d96db348b69a829fe6179dbf
Abhishek Nema <abhinema@gmail.com>
2018-10-28 23:46:04 +0530

    Update pub_sub.h

--- include/pub_sub.h
+++ include/pub_sub.h
@@ -60,12 +60,26 @@ public:
     };
     map<T, vector<SlotHandle>  > check;
     
+    dispatcher(){
+#ifdef DEBUG
+        cout<<"Called Dispatcher class Constructor"<<endl;
+#endif        
+    } 
+    ~dispatcher(){
+#ifdef DEBUG
+        cout<<"Called Dispatcher class Distructor"<<endl;
+#endif        
+    } 
+     
 public:
     Connection<T> subscribe(T i, std::function< void( T& )> b)  {
         auto id           = _nextID;
         SlotHandle handle = { id, b };
-        
         check[i].emplace_back( handle );
+#ifdef DEBUG
+        auto&& observers = check.at( i );
+        cout<<"registered event count:>>>"<<observers.size()<<"For Value:"<<i<<endl;
+#endif
         ++_nextID;
         return Connection<T>( this, id );
     }
@@ -89,8 +103,12 @@ public:
             return;
         }
         auto&& observers = check.at( event );
-        for( auto&& observer : observers )
+        for( auto&& observer : observers ){
+#ifdef DEBUG        
+            cout<<"Called:::"<<event<<endl;
+#endif            
             observer.slot( event );
+        }
     }
 };
 
@@ -106,25 +124,23 @@ void post_events(dispatcher<T> &d, T& val){
  Initialize Module class
  */
 template <typename T>
-Connection<T> init_Module(dispatcher<T>& d, T& val, std::function< void( T& )> func)  {
+Connection<T> init_Module(dispatcher<T>& d, T val, std::function< void( T& )> func)  {
     auto connection = d.subscribe( val, func );
     return connection;
 }
 /*
- Module class creates instance of dispatcher and assigns events with lambda function
+ Module class creates instance of dispatcher and assigns events with function
  */
 template <typename T>
 class Module{
     vector< Connection<T> > connections;
 public:
     template<std::size_t SIZE>
-    Module(dispatcher<T> &d, const array<T, SIZE> & eventList){
+    Module(dispatcher<T> &d, const array<T, SIZE> & eventList,std::function<void(T &)> fun){
         
         for(auto subscription_val: eventList){
-            connections.push_back(init_Module(d,subscription_val,
-                                              (std::function< void( T& )>) ([](T a){
-                cout<<"Received Event: "<<a<<endl;     }) )
-                                  );
+            cout<<"subscription_val: "<<subscription_val<<endl;
+            connections.push_back(init_Module(d,subscription_val, fun));
         }
     }
     ~Module(){

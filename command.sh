b63b0765f0570e3b1760fe39fd2955cc4ace9302
Abhishek Nema <abhinema@gmail.com>
2020-03-31 14:51:11 +0530

    updated file to kill recv_int after 5 sec

--- command.sh
+++ command.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # to run rcv & sender commands
-./build/bin/rcv_int 127.0.0.1 1234 &  
+./build/bin/rcv_int 127.0.0.1 1234 &
 ./build/bin/sender_int 127.0.0.1 1234 7
+sleep 5
+pkill rcv_int
